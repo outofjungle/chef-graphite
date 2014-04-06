45aa24681359cb27505e7e1df4c6e2a411749a66
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2014-04-06 16:10:18 -0700

    inital commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'chef-graphite'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef-graphite'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'graphite'
---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: chef-graphite
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+require_recipe "graphite::default"
